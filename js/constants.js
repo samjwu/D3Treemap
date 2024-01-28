16902328b6291655e03c9abe434a00eeb686b831
samjwu <sjwu@ualberta.ca>
2024-01-28 10:52:09 -0700

    Add legend consts

--- js/constants.js
+++ js/constants.js
@@ -2,3 +2,30 @@ const DATA_SOURCE = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tre
 
 const SVG_WIDTH = 1000;
 const SVG_HEIGHT = 600;
+
+const LEGEND_WIDTH = 500;
+const LEGEND_COLORS = [
+    '#FF0000',
+    '#FF9600',
+    '#F8FF00',
+    '#00FF00',
+    '#00FFF9',
+    '#5000FF',
+    '#B100FF',
+
+    '#BF0000',
+    '#BF7100',
+    '#BABF00',
+    '#00BF00',
+    '#00BFBB',
+    '#3C00BF',
+    '#8500BF',
+
+    '#800000',
+    '#804B00',
+    '#7C8000',
+    '#008000',
+    '#00807C',
+    '#280080',
+    '#580080',
+]
